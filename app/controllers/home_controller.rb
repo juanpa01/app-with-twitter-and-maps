1601889678abe608f944b8528ccb1bdcfc54545b
juanpa01 <juan-pablo961115@utp.edu.co>
2018-05-14 09:01:11 -0500

    part 2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,40 @@ class HomeController < ApplicationController
 
   client = SODA::Client.new({:domain => "www.datos.gov.co", :app_token => "F82AtyM4MKYkhVZTbTUYMKpPl"})
 
-  @results = client.get("xxxy-rt5e", {"$limit" => 5, "$where" => "departamento = 'CUNDINAMARCA'"})
+  @results_cundinamarca = client.get("xxxy-rt5e", {"$limit" =>1000, "$where" => "departamento = 'CUNDINAMARCA'"})
+  @results_antioquia = client.get("xxxy-rt5e", {"$limit" =>1000, "$where" => "departamento = 'ANTIOQUIA'"})
+  @results_valle_cauca = client.get("xxxy-rt5e", {"$limit" =>1000, "$where" => "departamento = 'VALLE'"})
+  @results_atlantico = client.get("xxxy-rt5e", {"$limit" =>1000, "$where" => "departamento = 'ATLÁNTICO'"})
+  @results_bolivar = client.get("xxxy-rt5e", {"$limit" =>1000, "$where" => "departamento = 'BOLÍVAR'"})
+  @results_norte_santander = client.get("xxxy-rt5e", {"$limit" =>1000, "$where" => "departamento = 'NORTE DE SANTANDER'"})
+  @results_risaralda =client.get("xxxy-rt5e", {"$limit" =>1000, "$where" => "departamento = 'RISARALDA'"})
+
+  @data_cundinamarca = 0.0
+  @data_antioquia = 0.0
+  @data_valle_cauca = 0.0
+  @data_atlantico = 0.0
+  @data_bolivar = 0.0
+  @data_norte_santander = 0.0
+  @data_risaralda = 0.0
+
+  for i in (0...1000) do
+    @data_cundinamarca += @results_cundinamarca[i][:cantidad].to_i
+    @data_antioquia += @results_antioquia[i][:cantidad].to_i
+    @data_valle_cauca += @results_valle_cauca[i][:cantidad].to_i
+    @data_atlantico += @results_atlantico[i][:cantidad].to_i
+    @data_bolivar += @results_bolivar[i][:cantidad].to_i
+    @data_norte_santander += @results_norte_santander[i][:cantidad].to_i
+    @data_risaralda += @results_risaralda[i][:cantidad].to_i
+  end
+
+  @radar = Gchart.pie(
+    :size => '600x400',
+    :title => 'Cantidad en gramos en los 7 principales departamentos de Colombia',
+    :labels => ['CUNDINAMARCA ', 'ANTIOQUIA', 'VALLE', 'ATLÁNTICO', 'BOLÍVAR', 'NORTE DE SANTANDER', 'RISARALDA'],
+    :custom => "chp=0.628",
+    :data => [@data_cundinamarca, @data_antioquia, @data_valle_cauca, @data_atlantico, @data_bolivar, @data_norte_santander, @data_risaralda]
+    )
+
   end
 
   def location_tweets
